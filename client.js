6da14052dcf0dea4d46d87184c40836baaa64be7
Atle Haugan <atleh@statoil.com>
2016-03-13 20:16:21 +0100

    Returning correct JSON when listing events

--- client.js
+++ client.js
@@ -1,26 +1,24 @@
-/**
- * Created by ATLEH on 19.02.2016.
- */
 var express = require('express');
 var http = require('http');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/events', function (req, res) {
     // var url =  'http://event.polarismedia.no/adressa/search/?dateFrom=2016-02-20&dateTo=2016-03-05&categories=8&page=1';
     var eventHost = 'event.polarismedia.no';
-    var eventPath = '/adressa/events/11948';
+    //var eventPath = '/adressa/events/11948';
+    var listEventsPath = '/adressa/search/?dateFrom=2016-03-13&dateTo=2016-03-17&categories=8&page=1';
     var optionsWithProxy = {
         host: "www-proxy.statoil.no",
         port: 80,
-        path: "www.statoil.no",
+        path: listEventsPath,
         headers: {
-            Host: "www.statoil.no"
+            Host: eventHost
         }
     };
     var optionsWithoutProxy = {
         host: eventHost,
         port: 80,
-        path: eventPath
+        path: listEventsPath
     };
 
     // Callback function is used to deal with response
@@ -34,10 +32,14 @@ app.get('/', function (req, res) {
         response.on('end', function () {
             // Data received completely.
             console.log(body);
+            var parsedJson = JSON.parse(body);
+            console.log(parsedJson);
+            res.send(parsedJson);
+            //res.send(body);
         });
     };
 // Make a request to the server
-    var req = http.request(optionsWithoutProxy, callback);
+    req = http.request(optionsWithProxy, callback);
     req.end();
 });
 
